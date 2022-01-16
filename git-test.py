53cf9a06d346a0b08f9c849e8a755b9db633299e
Dev Bekkevold <dev.bekkevold@gmail.com>
2022-01-16 21:48:47 +0100

    Add my 2022 goal

--- git-test.py
+++ git-test.py
@@ -1,2 +1,5 @@
-Learning_python = "Yey, this is my first commit to Github"
-print(Learning_python)
+learning_python = "Yey, this is my first commit to Github"
+print(learning_python)
+
+goal_for_2022 = "I will learn Python"
+print(goal_for_2022)
