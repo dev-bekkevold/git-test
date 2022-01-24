240f9a165b375b6ebc860c2c77e11cc3c26c3f87
Dev Bekkevold <dev.bekkevold@gmail.com>
2022-01-24 20:02:56 +0100

    make it more simple

--- git-test.py
+++ git-test.py
@@ -1,5 +1,8 @@
-learning_python = "Yey, this is my first commit to Github"
-print(learning_python)
+learning_python = "Yey, this is my first commit to Github. "
 
-goal_for_2022 = "I will learn Python"
-print(goal_for_2022)
+goal_for_2022 = "I will learn Python no matter what comes in my way :)"
+
+print(learning_python + goal_for_2022)
+
+
+
